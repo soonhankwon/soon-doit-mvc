fc4d4d612c5a64c402491d398e2302640c9169d4
soonhankwon <soonable@gmail.com>
2023-07-10 18:20:33 +0900

    Initial commit

---
+++ src/main/java/soon/soondoitmvc/SoonDoitMvcApplication.java
@@ -0,0 +1,13 @@
+package soon.soondoitmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoonDoitMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoonDoitMvcApplication.class, args);
+    }
+
+}
