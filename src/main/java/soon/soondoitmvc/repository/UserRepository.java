06fcfc3c44079e5b0dcb6e02275fcfbd5519ff6b
soonhankwon <soonable@gmail.com>
2023-07-11 00:38:41 +0900

    feat : findUserByName

--- src/main/java/soon/soondoitmvc/repository/UserRepository.java
+++ src/main/java/soon/soondoitmvc/repository/UserRepository.java
@@ -3,5 +3,8 @@ package soon.soondoitmvc.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import soon.soondoitmvc.domain.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByName(String name);
 }
