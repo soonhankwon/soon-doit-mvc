95ef141a1201c7ccddfe7d75a243044bcdc74b5b
soonhankwon <soonable@gmail.com>
2023-07-11 17:12:00 +0900

    feat : mission repository

--- src/main/java/soon/soondoitmvc/repository/MissionRepository.java
+++ src/main/java/soon/soondoitmvc/repository/MissionRepository.java
@@ -1,7 +1,9 @@
 package soon.soondoitmvc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import soon.soondoitmvc.domain.Mission;
 
+@Repository
 public interface MissionRepository extends JpaRepository<Mission, Long> {
 }
