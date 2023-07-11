9ca9b139df6e4557035db318635fad48cdef52e5
soonhankwon <soonable@gmail.com>
2023-07-11 17:12:11 +0900

    feat : query 수정

--- src/main/java/soon/soondoitmvc/repository/UserRepository.java
+++ src/main/java/soon/soondoitmvc/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package soon.soondoitmvc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import soon.soondoitmvc.domain.User;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findUserByName(String name);
+
+    Optional<User> findUserByNameAndPassword(String name, String password);
 }
