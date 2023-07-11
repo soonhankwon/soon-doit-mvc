961300f0b55bdd8e182c6154049188ebbc66a708
soonhankwon <soonable@gmail.com>
2023-07-11 17:11:49 +0900

    feat : validate

--- src/main/java/soon/soondoitmvc/dto/LoginReqDto.java
+++ src/main/java/soon/soondoitmvc/dto/LoginReqDto.java
@@ -2,11 +2,15 @@ package soon.soondoitmvc.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 public class LoginReqDto {
 
+    @NotEmpty
     private String name;
 
+    @NotEmpty
     private String password;
 
     public LoginReqDto(String name, String password) {
--- src/main/java/soon/soondoitmvc/dto/SignupReqDto.java
+++ src/main/java/soon/soondoitmvc/dto/SignupReqDto.java
@@ -2,10 +2,15 @@ package soon.soondoitmvc.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 public class SignupReqDto {
 
+    @NotEmpty
     private String name;
+
+    @NotEmpty
     private String password;
 
     public SignupReqDto(String name, String password) {
