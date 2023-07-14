f7c18816a176badf46845c1bf020f6146e35ec1f
soonhankwon <soonable@gmail.com>
2023-07-14 09:53:01 +0900

    config : LoginUserArgument Resolver 추가

--- src/main/java/soon/soondoitmvc/config/WebConfig.java
+++ src/main/java/soon/soondoitmvc/config/WebConfig.java
@@ -1,9 +1,13 @@
 package soon.soondoitmvc.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import soon.soondoitmvc.argumentresolver.LoginUserArgumentResolver;
 import soon.soondoitmvc.interceptor.LoginCheckInterceptor;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -15,5 +19,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**")
                 .excludePathPatterns("/", "/users/add", "/login", "/logout",
                         "/css/**", "/*.ico", "/error");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginUserArgumentResolver());
     }
 }
