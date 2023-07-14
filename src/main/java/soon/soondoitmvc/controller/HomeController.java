dc7cf22c0732ad9f988778fe57b47d14b1446799
soonhankwon <soonable@gmail.com>
2023-07-14 09:52:18 +0900

    feat : @Login 애노테이션 & ArgumentResolver 사용으로 수정

--- src/main/java/soon/soondoitmvc/controller/HomeController.java
+++ src/main/java/soon/soondoitmvc/controller/HomeController.java
@@ -1,19 +1,16 @@
 package soon.soondoitmvc.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import soon.soondoitmvc.argumentresolver.Login;
 import soon.soondoitmvc.domain.User;
-import soon.soondoitmvc.session.SessionConst;
 
 @Controller
-@RequiredArgsConstructor
 public class HomeController {
 
     @GetMapping("/")
-    public String home(@SessionAttribute(name = SessionConst.LOGIN_USER, required = false) User loginUser, Model model) {
+    public String home(@Login User loginUser, Model model) {
         if(loginUser == null) {
             return "home";
         }
